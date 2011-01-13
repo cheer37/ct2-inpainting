5d967258a33fb52861566e1f2773fe08f0c5949e
inoueediv <inoueediv@5e727123-e3d5-56dc-acd8-72af5541568b>
2011-01-13 11:56:15 +0000

    Ajout image resultat, et autre trucs trop cooool :o

--- trunk/mainwindow.cpp
+++ trunk/mainwindow.cpp
@@ -9,6 +9,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     this->masque = 0;
     this->image = 0;
+    this->resultat = 0;
 }
 
 MainWindow::~MainWindow()
@@ -41,13 +42,17 @@ void MainWindow::on_actionOuvrir_une_image_triggered()
 
     if (!filename.isEmpty())
     {
+        this->ui->checkBox_masque->setChecked(true);
+        this->ui->checkBox_res->setChecked(false);
         this->image = new CImage(filename);
         scene = new QGraphicsScene();
         scene->addItem(this->image);
-        this->masque = new CImage(this->image->width(), this->image->height(), 1);
-        scene->addItem(this->masque);
+
+        this->masque = new CImage(this->image->width(), this->image->height(), 1);//1 pour image mono (masque) ARGB
         this->masque->set_pen_size(ui->spinBox_size->value());
         this->masque->set_pen_mode(1);
+        scene->addItem(this->masque);
+        this->resultat = new CImage(this->image->width(), this->image->height(), 0);//0 pour image normale RGB
         ui->graphicsView_img->setScene(scene);
     }
 }
@@ -80,7 +85,7 @@ void MainWindow::on_actionEnregistrer_l_image_triggered()
         this->scene->render(&painter);
         painter.end();
         img.save(Path);*/
-        this->image->save(Path);
+        this->resultat->save(Path);
     }
 }
 
@@ -137,6 +142,18 @@ void MainWindow::on_checkBox_masque_stateChanged(int a)
     }
 }
 
+
+void MainWindow::on_checkBox_res_stateChanged(int a)
+{
+    if (this->resultat != 0)
+    {
+        if (a)
+            scene->addItem(this->resultat);
+        else
+            scene->removeItem(this->resultat);
+    }
+}
+
 void MainWindow::on_toolButton_write_clicked()
 {
     if (this->masque != 0)
@@ -154,3 +171,28 @@ void MainWindow::on_toolButton_erase_clicked()
 }
 
 
+
+/*
+  A modifier pr lancer le traitement sur les images, ici je fusionne juste l'image et le masque dans resultat pour exemple
+  */
+void MainWindow::on_pushButton_go_clicked()
+{
+    if (this->image != 0 && this->resultat != 0)// si image et resultat instanciées
+    {
+        if (this->ui->radioButton_1->isChecked())//si methode 1 coché
+        {
+            for (int i = 0; i < this->image->width(); ++i) {
+                for(int j = 0; j < this->image->height(); ++j) {
+                    if (this->masque->getPixel(i, j) == qRgba(0, 0, 0, 255))
+                        this->resultat->setPixel(i, j, qRgb(0, 0, 0));
+                    else
+                        this->resultat->setPixel(i, j, this->image->getPixel(i, j));
+                }
+            }
+        }
+        else if (this->ui->radioButton_2->isChecked())
+        {
+            QMessageBox::information(this, "Attention", "Methode 2 non implementee!") ;
+        }
+    }
+}
