05ea68da86da289eec9cc7480375722aaa46e592
inoueediv <inoueediv@5e727123-e3d5-56dc-acd8-72af5541568b>
2011-01-27 15:24:35 +0000

    Allo

--- ctschumperle.cpp
+++ ctschumperle.cpp
@@ -9,10 +9,11 @@ void CTschumperle::appliquer(CImage *init, CImage *masque, CImage *out, float pa
 {
     int w = init->width();
     this->progressbar->setMaximum(w);//initialise la progressbar
-    for (int i = 0; i < init->width(); ++i) {
+    for (int i = 0; i < init->width(); ++i)
+    {
         this->progressbar->setValue(this->progressbar->value()+1);//incremente la progressbar
-        for(int j = 0; j < init->height(); ++j) {
-
+        for(int j = 0; j < init->height(); ++j)
+        {
             if (masque->getPixel(i, j) == qRgba(0, 0, 0, 255))
                 out->setPixel(i, j, qRgb(0, 0, 0));
             else
