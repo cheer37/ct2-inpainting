21bd8b22d4ebd9d1ffd6dee3299d44b33a18ee4f
vergeot.yoan@gmail.com <vergeot.yoan@gmail.com@5e727123-e3d5-56dc-acd8-72af5541568b>
2011-02-14 23:47:01 +0000

    Modification methode 2

--- trunk/cmgvc.cpp
+++ trunk/cmgvc.cpp
@@ -4,40 +4,56 @@ CMgvc::CMgvc()
 {
 }
 
+bool isOnEdgeOfOmega (CImage * masque, int x, int y)
+{
+	for (int j = y-1; j <= y+1; ++j)
+		for (int i = x-1; i <= x+1; ++i)
+			if (masque->getPixel(i, j) == qRgba(255, 255, 255, 0))
+				return true;
+
+	return false;
+}
+
 void CMgvc::appliquer(CImage *init, CImage *masque, CImage *out, float nb_iter, float delta_t)
 {
 
     this->progressbar->setMaximum(nb_iter);
-qDebug() << "coucou1\n";
+
     CMatriMage * Laplacien = new CMatriMage(init->width(), init->height());
     CMatriMage * Gradient  = new CMatriMage(init->width(), init->height());
     CMatriMage * SobelX    = new CMatriMage(init->width(), init->height());
     CMatriMage * SobelY    = new CMatriMage(init->width(), init->height());
-qDebug() << "coucou2\n";
+
     CImage * temp = new CImage(init->width(), init->height(), 0);
-qDebug() << "coucou3\n";
+    CImage * masqueTemp = new CImage(init->width(), init->height(), 1);
+	CImage * valEr;
+
     CVector2f deltaLr, deltaLg, deltaLb;
     CVector2f Nr, Ng, Nb;
     int R, G, B;
-qDebug() << "coucou4\n";
+
+	masqueTemp->Copy(masque);
     out->Copy(init);
-qDebug() << "coucou5\n";
+
     for (int n = 0; n < nb_iter; ++n)
     {
+		valEr = new CImage(init->width(), init->height(), 0);
         this->progressbar->setValue(this->progressbar->value()+1);//incremente la progressbar
-    qDebug() << "allo1\n";
+
         temp->Copy(out);
-    qDebug() << "allo2\n";
+
         Laplacien->Laplacien(temp);
         Gradient->Gradient(temp, SobelX, SobelY);
-    qDebug() << "allo3\n";
 
-        for (int j = 0; j < masque->height(); ++j)
+
+        for (int j = 0; j < masqueTemp->height(); ++j)
         {
-            for (int i = 0; i < masque->width(); ++i)
+            for (int i = 0; i < masqueTemp->width(); ++i)
             {
-                if (masque->getPixel(i, j) == qRgba(0, 0, 0, 255))
+				if (masqueTemp->getPixel(i, j) == qRgba(0, 0, 0, 255) /*&& isOnEdgeOfOmega(masque, j, i)*/)
                 {
+					valEr->setPixel(i, j, qRgb(0,0,0));
+
                     deltaLr.SetVal(Laplacien->GetVal(i+1,j,0) - Laplacien->GetVal(i-1,j,0), Laplacien->GetVal(i+1,j,0) - Laplacien->GetVal(i-1,j,0));
                     deltaLg.SetVal(Laplacien->GetVal(i+1,j,1) - Laplacien->GetVal(i-1,j,1), Laplacien->GetVal(i+1,j,1) - Laplacien->GetVal(i-1,j,1));
                     deltaLb.SetVal(Laplacien->GetVal(i+1,j,2) - Laplacien->GetVal(i-1,j,2), Laplacien->GetVal(i+1,j,2) - Laplacien->GetVal(i-1,j,2));
@@ -53,10 +69,23 @@ qDebug() << "coucou5\n";
                     R = temp->getRedPixel(i,j)   + delta_t*(deltaLr*Nr*Gradient->GetVal(i,j, 0));
                     G = temp->getGreenPixel(i,j) + delta_t*(deltaLg*Ng*Gradient->GetVal(i,j, 1));
                     B = temp->getBluePixel(i,j)  + delta_t*(deltaLb*Nb*Gradient->GetVal(i,j, 2));
+
                     out->setPixel(i, j, qRgb(R,G,B));
                 }
             }
         }
+
+		for (int j = 0; j < masqueTemp->height(); ++j)
+		{
+			for (int i = 0; i < masqueTemp->width(); ++i)
+			{
+				if (valEr->getPixel(i, j) == qRgb(0, 0, 0))
+				{
+					masqueTemp->setPixel(i, j, qRgba (255,255,255, 0));
+				}
+			}
+		}
+		delete valEr;
     }
 }
 
